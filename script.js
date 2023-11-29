337fa5fb77f804c6c639e1f4b7d429acaaf341e7
Ali ihsan Ertugrul <ertugrulali3538@gmail.com>
2023-11-29 19:51:14 +0300

    data import edildi

---
+++ script.js
@@ -0,0 +1,10 @@
+import { newsData } from "./data/news.js"; 
+
+const loadData = () => {
+    for (let i = 0; i < newsData.length; i++) {
+        newsData[i];
+        console.log(newsData[i].title)
+        
+    }
+}
+loadData();
